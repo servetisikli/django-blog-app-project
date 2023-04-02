b5005b1bba10198cd59588452f5ea004fadc9f1e
servetisikli <servetisikli@gmail.com>
2023-04-02 06:05:10 +0200

    media edit_new

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+def user_directory_path(instance, filename):
+    return 'blog/{0}/{1}'.format(instance.author.id, filename)
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
@@ -17,7 +20,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField(upload_to=)
+    image = models.ImageField(upload_to=user_directory_path, default='django.jpg')
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
