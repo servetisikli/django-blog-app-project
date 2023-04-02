7c1740e47104d9f0ad393bbf13c8c67f9b735b08
servetisikli <servetisikli@gmail.com>
2023-04-02 05:53:28 +0200

    media settings

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to=)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
