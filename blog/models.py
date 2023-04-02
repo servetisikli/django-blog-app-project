d16f66aa2aab7ddcd867775a2b5b724f2ac5b5e3
servetisikli <servetisikli@gmail.com>
2023-04-02 06:07:10 +0200

    django.png

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField(upload_to=user_directory_path, default='django.jpg')
+    image = models.ImageField(upload_to=user_directory_path, default='django.png')
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
