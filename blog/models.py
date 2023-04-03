b57538341a6cf5d953d09726d1d7a1c4744217cb
servet-isikli <servetisikli@yahoo.com>
2023-04-03 10:27:58 +0200

    signals, models...

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 def user_directory_path(instance, filename):
     return 'blog/{0}/{1}'.format(instance.author.id, filename)
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
@@ -20,16 +21,17 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    image = models.ImageField(upload_to=user_directory_path, default='django.png')
+    image = models.ImageField(
+        upload_to=user_directory_path, default='django.png')
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    STATUS = (
+    OPTIONS = (
         ('d', 'Draft'),
         ('p', 'Publish')
     )
-    status = models.CharField(max_length=10, choices=STATUS, default='d')
+    status = models.CharField(max_length=10, choices=OPTIONS, default='d')
     slug = models.SlugField(blank=True, unique=True)
 
     class Meta:
@@ -38,3 +40,31 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+    
+    
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+        
+    def __str__(self):
+        return self.username
+        
+
+class PostView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
