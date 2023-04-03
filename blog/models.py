5257f89ef2cc7b0f10ff1f4b05419828c984ea74
servetisikli <servetisikli@gmail.com>
2023-04-03 10:25:46 +0200

    signals, models...

--- blog/models.py
+++ blog/models.py
@@ -27,11 +27,11 @@ class Post(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    STATUS = (
+    OPTIONS = (
         ('d', 'Draft'),
         ('p', 'Publish')
     )
-    status = models.CharField(max_length=10, choices=STATUS, default='d')
+    status = models.CharField(max_length=10, choices=OPTIONS, default='d')
     slug = models.SlugField(blank=True, unique=True)
 
     class Meta:
@@ -40,3 +40,31 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+    
+    
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+        
+    def __str__(self):
+        return self.username
+        
+
+class PostView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
