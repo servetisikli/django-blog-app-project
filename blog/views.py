15ddbbae09bf64a82d15acd5878aca9e47317a02
servetisikli <servetisikli@gmail.com>
2023-04-13 17:36:36 +0200

    edited slug - created utils.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     qs = Post.objects.filter(status='p')
@@ -7,3 +8,19 @@ def post_list(request):
         'object_list' : qs
     }
     return render (request, "blog/post_list.html", context )
+
+
+def post_create(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("blog:list")
+    context = {
+        'form': form
+    }
+    return render(request, "blog/post_create.html", context )
+            
