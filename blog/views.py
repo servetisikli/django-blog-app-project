8c0b69e16a576de674f2f8cb8f9906505557b5c1
servet-isikli <servetisikli@yahoo.com>
2023-04-15 21:27:16 +0200

    added templates

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Post
 from .forms import PostForm
+
 
 def post_list(request):
     qs = Post.objects.filter(status='p')
     context = {
-        'object_list' : qs
+        'object_list': qs
     }
-    return render (request, "blog/post_list.html", context )
+    return render(request, "blog/post_list.html", context)
 
 
 def post_create(request):
@@ -22,5 +23,37 @@ def post_create(request):
     context = {
         'form': form
     }
-    return render(request, "blog/post_create.html", context )
-            
+    return render(request, "blog/post_create.html", context)
+
+
+def post_detail(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_detail.html", context)
+
+
+def post_update(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect('blog:list')
+
+    context = {
+        "object": obj,
+        "form": form
+    }
+    return render(request, "blog/post_update.html", context)
+
+
+def post_delete(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    if request.method == "POST":
+        obj.delete()
+        return redirect("blog:list")
+    context = {
+        "object":obj
+    }
+    return render(request, "post/post.delete.html", context)
