c3682dbe6f665456b4f7556a297dd8a4a566802c
servetisikli <servetisikli@gmail.com>
2023-04-15 21:25:27 +0200

    added static

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def comment_count(self):
+        return self.comment_set.all().count()
+
+    def view_count(self):
+        return self.postview_set.all().count()
+
+    def like_count(self):
+        return self.like_set.all().count()
+
+    def comments(self):
+        return self.comment_set.all()
 
 
 class Post(models.Model):
